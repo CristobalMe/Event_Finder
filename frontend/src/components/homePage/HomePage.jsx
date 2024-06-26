1f599bbd8a7464a3797d43ce77b1e029212b9c51
Cristobal Medina Meza <cristobalmedina@meta.com>
2024-06-26 15:08:10 -0700

    Added a swiper and a coverflow

--- frontend/src/components/homePage/HomePage.jsx
+++ frontend/src/components/homePage/HomePage.jsx
@@ -1,11 +1,23 @@
+import CoverFlow from '../swiper/CoverFlow'
+import Carousel from '../carousel/Carousel'
 import Header from '../header/Header'
 
 function App() {
 
-
   return (
     <div>
-      <Header />
+        <Header />
+
+
+        <div className='ml-[7rem] mr-[7rem]  mt-[10rem]'>
+            <h2 className='font-bebas text-white text-xl'>For You</h2>
+            <Carousel />
+        </div>
+
+        <div className='ml-[7rem] mr-[7rem]  mt-[10rem] content-center'>
+            <CoverFlow />
+        </div>
+    
 
     </div>
   )
