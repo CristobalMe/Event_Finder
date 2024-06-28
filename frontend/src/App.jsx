d559018c0848fdcc6fc7f0757a6b9c59aeff806f
Cristobal Medina Meza <cristobalmedina@meta.com>
2024-06-28 13:47:51 -0700

    initial backend: initial models for event and user, working on sign in/up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,24 +5,54 @@ import EventAttendingPage from "./components/eventAttendingPage/EventAttendingPa
 import FavoriteEventsPage from "./components/favoriteEventsPage/FavoriteEventsPage.jsx"
 import InitialPage from "./components/initialPage/InitialPage.jsx"
 import SignupPage from "./components/signUpPage/SignUpPage.jsx"
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+{/* testing for user auth */}
+import LoginForm from "./components/LoginForm/LoginForm.jsx"
+import SignupForm from "./components/SignupForm/SignupForm.jsx"
+import { useState, useEffect } from 'react';
+import { UserContext } from './UserContext';
+
 
 function App() {
 
+  const [user, setUser] = useState(() => {
+    // Retrieve the user data from storage or set it to null if not found
+    const storedUser = localStorage.getItem('user');
+    // return storedUser ? JSON.parse(storedUser) : null;
+    return null
+  });
+
+  const updateUser = (newUser) => {
+    setUser(newUser);
+  };
+
+  useEffect(() => {
+    // Save the user data to storage whenever the user state changes
+    localStorage.setItem('user', JSON.stringify(user));
+  }, [user]);
+
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<InitialPage />} />
-        <Route path="/SignUp" element={<SignupPage />} />
-        <Route path="/Home" element={<HomePage />} />
-        <Route path="/ForYou" element={<ForYouPage />} />
-        <Route path="/Nearby" element={<NearbyPage />} />
-        <Route path="/Attending" element={<EventAttendingPage />} />
-        <Route path="/Favorites" element={<FavoriteEventsPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <UserContext.Provider value={{ user, updateUser }}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<InitialPage />} />
+            <Route path="/SignUp" element={<SignupPage />} />
+            <Route path="/Home" element={<HomePage />} />
+            <Route path="/ForYou" element={<ForYouPage />} />
+            <Route path="/Nearby" element={<NearbyPage />} />
+            <Route path="/Attending" element={<EventAttendingPage />} />
+            <Route path="/Favorites" element={<FavoriteEventsPage />} />
+
+            {/* testing for user auth */}
+            <Route path="/1" element={<LoginForm />} />
+            <Route path="/2" element={<SignupForm />} />
+          </Routes>
+        </Router>
+      </UserContext.Provider>
+    </div>
   )
 }
 
--- frontend/src/components/eventAttendingPage/EventAttendingPage.jsx
+++ frontend/src/components/eventAttendingPage/EventAttendingPage.jsx
@@ -1,5 +1,5 @@
 import Header from '../header/Header.jsx'
-import CardGridAttending from "./CardGridAttending.jsx"
+import CardGridAttending from "./CardGridEventAttending.jsx"
 
 function App() {
 
