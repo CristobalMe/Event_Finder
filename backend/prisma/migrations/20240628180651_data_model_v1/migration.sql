c62008159e2c658f9ea5a6fec8581daa61dd7cdc
Cristobal Medina Meza <cristobalmedina@meta.com>
2024-06-28 13:47:51 -0700

    initial backend: initial models for event and user, working on sign in/up

--- backend/prisma/migrations/20240628180651_data_model_v1/migration.sql
+++ backend/prisma/migrations/20240628180651_data_model_v1/migration.sql
@@ -11,3 +11,13 @@ CREATE TABLE "event" (
 
     CONSTRAINT "event_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
