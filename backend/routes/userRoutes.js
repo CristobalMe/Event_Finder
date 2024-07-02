a760e11db464a8b3b46360c26fb82c5bcfbcb7c0
Cristobal Medina Meza <cristobalmedina@meta.com>
2024-07-02 12:43:16 -0700

    Initial backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,93 +1,95 @@
-const express = require("express");
-const router = express.Router();
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-const bcrypt = require("bcrypt");
+const express = require('express')
+const router = express.Router()
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+const bcrypt = require('bcrypt');
 
-router.get("/", async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
+router.get('/', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
 
-router.post("/", async (req, res) => {
-  const { username, email, password } = req.body;
+router.post('/', async (req, res) => {
+  const {  username, email, password  } = req.body
 
-  const existingUser = await prisma.user.findFirst({
-    where: { username: username },
-  });
+  const existingUser = await prisma.user.findFirst({ where: { username: username } });
   if (existingUser) {
-    return res.status(400).json({ error: "Username already exists" });
+    return res.status(400).json({ error: 'Username already exists' });
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const hashedPasswordValue = String(hashedPassword);
-
+  
   const newUser = await prisma.user.create({
     data: {
-      username,
-      email,
-      password: hashedPasswordValue,
-    },
-  });
+        username, 
+        email, 
+        password: hashedPasswordValue
+    }
+  })
 
   req.session.user = newUser;
 
-  res.json(newUser);
-});
+  res.json(newUser)
+})
 
-router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { username, email, password } = req.body;
+router.put('/:id', async(req, res) => {
+  const { id } = req.params
+  const {  username, email, password  } = req.body
 
   const updatedUser = await prisma.user.update({
     where: { id: parseInt(id) },
     data: {
-      username,
-      email,
-      password,
-    },
-  });
+        username, 
+        email, 
+        password
+    }
+  })
 
-  res.json(updatedUser);
-});
+  res.json(updatedUser)
+})
 
-router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
+router.delete('/:id', async(req, res) => {
+  const { id } = req.params
 
   const deletedUser = await prisma.user.delete({
-    where: { id: parseInt(id) },
-  });
+    where: { id: parseInt(id) }
+  })
 
-  res.json(deletedUser);
+  res.json(deletedUser)
+
+})
+
+
+
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+      // Find the user by username
+      const User = await prisma.user.findFirst({ where: { username: username } });
+  
+      if (!User) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+      } 
+  
+      // Compare the password
+      const isValidPassword = await bcrypt.compare(password, User.password);
+  
+      if (!isValidPassword) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+      }
+  
+      // Set the user in the session
+      req.session.user = User;
+  
+      // Return the user data in the response
+      res.json({ User });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Server error' });
+    }
 });
 
-router.post("/login", async (req, res) => {
-  const { username, password } = req.body;
 
-  try {
-    // Find the user by username
-    const user = await prisma.user.findFirst({ where: { username: username } });
-
-    if (!user) {
-      return res.status(401).json({ error: "Invalid username or password" });
-    }
-
-    // Compare the password
-    const isValidPassword = await bcrypt.compare(password, user.password);
-
-    if (!isValidPassword) {
-      return res.status(401).json({ error: "Invalid username or password" });
-    }
-
-    // Set the user in the session
-    req.session.user = user;
-
-    // Return the user data in the response
-    res.json({ user });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Server error" });
-  }
-});
-
-module.exports = router;
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,61 +1,62 @@
-const cors = require("cors");
-const morgan = require("morgan");
-const express = require("express");
-const SequelizeStoreInit = require("connect-session-sequelize");
-const session = require("express-session");
-const Sequelize = require("sequelize");
-const app = express();
-const PORT = 3000;
+const cors = require('cors');
+const morgan = require('morgan');
+const express = require('express');
+const SequelizeStoreInit = require('connect-session-sequelize');
+const session = require('express-session');
+const Sequelize = require('sequelize');
+const app = express()
+const PORT = 3000
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
-app.use(express.json());
-app.use(morgan());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+app.use(express.json())
+app.use(morgan())
 
-const newSequelize = new Sequelize("event_finder", "postgres", "1025323", {
-  host: "localhost",
-  dialect: "postgres",
+const newSequelize = new Sequelize('event_finder', 'postgres', '1025323', {
+    host: 'localhost',
+    dialect: 'postgres'
 });
 
 const SequelizeStore = SequelizeStoreInit(session.Store);
 const sessionStore = new SequelizeStore({
-  db: newSequelize,
+  db: newSequelize
 });
 
 // Session middleware
 app.use(
   session({
-    secret: "your-secret-key",
+    secret: 'your-secret-key',
     resave: false,
     saveUninitialized: false,
     store: sessionStore,
     cookie: {
       sameSite: false,
       secure: false,
-      expires: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000), // 1 year in milliseconds
-    },
-  }),
+      expires: new Date(Date.now() + (365 * 24 * 60 * 60 * 1000)) // 1 year in milliseconds
+    }
+  })
 );
 sessionStore.sync();
 
-const eventRoutes = require("./routes/eventRoutes");
-const userRoutes = require("./routes/userRoutes");
+const eventRoutes = require("./routes/eventRoutes")
+const userRoutes = require("./routes/userRoutes")
 
-app.use("/event", eventRoutes);
-app.use("/users", userRoutes);
+app.get('/', (req, res) => {
+    res.send('Welcome to my app!')
+})
 
-newSequelize
-  .sync({ alter: true })
+app.use('/event', eventRoutes)
+app.use('/users', userRoutes)
+
+newSequelize.sync({ alter: true })
   .then(() => {
     const port = 3000;
     app.listen(port, () => {
       console.log(`App is listening on port ${port}`);
     });
   })
-  .catch((error) => {
-    console.error("Unable to connect to the database:", error);
+  .catch(error => {
+    console.error('Unable to connect to the database:', error);
   });
