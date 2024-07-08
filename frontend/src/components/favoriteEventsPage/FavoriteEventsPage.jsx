04e475e230b81e6d0ecf824b3cb2a2668bece551
Cristobal Medina Meza <cristobalmedina@meta.com>
2024-07-08 16:20:13 -0700

    “message”

--- frontend/src/components/favoriteEventsPage/FavoriteEventsPage.jsx
+++ frontend/src/components/favoriteEventsPage/FavoriteEventsPage.jsx
@@ -1,15 +1,15 @@
-import Header from "../header/Header.jsx";
-import CardGridFavoriteEvents from "./CardGridFavoriteEvents.jsx";
+import Header from '../header/Header.jsx'
+import CardGridFavoriteEvents from "./CardGridFavoriteEvents.jsx"
 
 function App() {
+
   return (
     <div>
-      <Header />
-      <div className="h-screen flex items-center justify-center lg:mt-[20rem] sm:mt-[45rem]">
-        <CardGridFavoriteEvents />
-      </div>
+
+        <Header />
+        <div className="h-screen flex items-center justify-center lg:mt-[20rem] sm:mt-[45rem]"><CardGridFavoriteEvents /></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
