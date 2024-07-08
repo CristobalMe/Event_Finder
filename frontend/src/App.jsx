2a6dd54b008f34d750bb7f185f08535c71e6784c
Cristobal Medina Meza <112215849+CristobalMe@users.noreply.github.com>
2024-07-08 09:28:23 -0700

    Prettier setup (#12)
    * Prettier settup
    
    * fixing prettier and husky
    
    * bad changes
    
    ---------
    
    Co-authored-by: Cristobal Medina Meza <cristobalmedina@meta.com>
    Co-authored-by: Yicheng (Jerry) Gong <8813928+ygongdev@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,33 @@
-import HomePage from "./components/homePage/HomePage.jsx"
-import ForYouPage from "./components/forYouPage/ForYouPage.jsx"
-import NearbyPage from "./components/nearbyPage/NearbyPage.jsx"
-import EventsAttendingPage from "./components/eventsAttendingPage/EventsAttendingPage.jsx"
-import FavoriteEventsPage from "./components/favoriteEventsPage/FavoriteEventsPage.jsx"
-import InitialPage from "./components/initialPage/InitialPage.jsx"
+import HomePage from './components/homePage/HomePage.jsx'
+import ForYouPage from './components/forYouPage/ForYouPage.jsx'
+import NearbyPage from './components/nearbyPage/NearbyPage.jsx'
+import EventsAttendingPage from './components/eventsAttendingPage/EventsAttendingPage.jsx'
+import FavoriteEventsPage from './components/favoriteEventsPage/FavoriteEventsPage.jsx'
+import InitialPage from './components/initialPage/InitialPage.jsx'
 
-import SignupPage from "./components/SignupPage/SignUpPage.jsx"
+import SignupPage from './components/SignupPage/SignUpPage.jsx'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-// TODO: 
+// TODO:
 // login/signup routing logic
 // Consolidate Headers in to one
 // Fix sign in and sign up the same forms (User flow)
 // Fix Carousel data flow
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<InitialPage />} />
-        <Route path="/SignUp" element={<SignupPage />} />
-        <Route path="/Home" element={<HomePage />} />
-        <Route path="/ForYou" element={<ForYouPage />} />
-        <Route path="/Nearby" element={<NearbyPage />} />
-        <Route path="/Attending" element={<EventsAttendingPage />} />
-        <Route path="/Favorites" element={<FavoriteEventsPage />} />
-      </Routes>
-    </Router>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<InitialPage />} />
+                <Route path="/SignUp" element={<SignupPage />} />
+                <Route path="/Home" element={<HomePage />} />
+                <Route path="/ForYou" element={<ForYouPage />} />
+                <Route path="/Nearby" element={<NearbyPage />} />
+                <Route path="/Attending" element={<EventsAttendingPage />} />
+                <Route path="/Favorites" element={<FavoriteEventsPage />} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App
