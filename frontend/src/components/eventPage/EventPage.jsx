1e5334aad4e156b686c0d3dce3b1bdbf0ddc4df3
Cristobal Medina Meza <112215849+CristobalMe@users.noreply.github.com>
2024-07-10 13:57:29 -0700

    Added a loading screen, to display something while the data is being fetched (#24)

--- frontend/src/components/eventPage/EventPage.jsx
+++ frontend/src/components/eventPage/EventPage.jsx
@@ -1,48 +1,61 @@
-import React, { useState, useEffect } from "react";
-import Header from "../header/Header";
-import DisplayEventInfo from "./DisplayEventInfo";
-import DisplayEventComments from "./DisplayEventComments";
+import { useState, useEffect } from 'react'
+import Header from '../header/Header'
+import DisplayEventInfo from './DisplayEventInfo'
+import DisplayEventComments from './DisplayEventComments'
+import LoadingPage from '../loadingPage/LoadingPage'
 
 const EventPage = () => {
-  const current_link = window.location.pathname;
-  const current_eventId = current_link.split("/")[2];
-  const [event, setEvent] = useState();
-  const [comments, setComments] = useState();
-  let [changeInEvent, setchangeInEvent] = useState(true);
+    const current_link = window.location.pathname
+    const current_eventId = current_link.split('/')[2]
+    const [event, setEvent] = useState()
+    const [comments, setComments] = useState()
+    let [changeInEvent, setchangeInEvent] = useState(true)
+    const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    if (changeInEvent) {
-      fetchEvent();
-      fetchComments();
+    useEffect(() => {
+        if (changeInEvent) {
+            setIsLoading(true)
+            fetchEvent()
+            fetchComments()
+        }
+        setIsLoading(false)
+        setchangeInEvent(false)
+    }, [event])
+
+    const fetchEvent = () => {
+        fetch(`http://localhost:3000${current_link}`)
+            .then((response) => response.json())
+            .then((data) => setEvent(data))
+            .catch((error) => console.error('Error fetching:', error))
     }
-    setchangeInEvent(false);
-  }, [event]);
 
-  const fetchEvent = () => {
-    fetch(`http://localhost:3000${current_link}`)
-      .then((response) => response.json())
-      .then((data) => setEvent(data))
-      .catch((error) => console.error("Error fetching:", error));
-  };
+    const fetchComments = () => {
+        fetch(`http://localhost:3000/comments/${current_eventId}`)
+            .then((response) => response.json())
+            .then((data) => setComments(data))
+            .catch((error) => console.error('Error fetching:', error))
+    }
 
-  const fetchComments = () => {
-    fetch(`http://localhost:3000/comments/${current_eventId}`)
-      .then((response) => response.json())
-      .then((data) => setComments(data))
-      .catch((error) => console.error("Error fetching:", error));
-  };
+    return (
+        <div>
+            {!isLoading && (
+                <div>
+                    <Header />
+                    <div className="h-screen flex items-center justify-center">
+                        <DisplayEventInfo event={event} />
+                    </div>
+                    <div className="h-screen flex items-center justify-center">
+                        <DisplayEventComments comments={comments} />
+                    </div>
+                </div>
+            )}
+            {isLoading && (
+                <div>
+                    <LoadingPage />
+                </div>
+            )}
+        </div>
+    )
+}
 
-  return (
-    <div>
-      <Header />
-      <div className="h-screen flex items-center justify-center">
-        <DisplayEventInfo event={event} />
-      </div>
-      <div className="h-screen flex items-center justify-center">
-        <DisplayEventComments comments={comments} />
-      </div>
-    </div>
-  );
-};
-
-export default EventPage;
+export default EventPage
