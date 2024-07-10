d2fefc64d36de4086e66c7726b7534ea0eae8a3f
Cristobal Medina Meza <112215849+CristobalMe@users.noreply.github.com>
2024-07-10 11:30:20 -0700

    Added the filter feature to the frontend

--- frontend/src/components/homePage/HomePage.jsx
+++ frontend/src/components/homePage/HomePage.jsx
@@ -1,52 +1,119 @@
-import EventsCarousel from "../carousel/EventsCarousel";
-import Header from "../header/Header";
-import SpinningBanner from "../spinningBanner/SpinningBaner";
-import { useState, useEffect } from "react";
+import EventsCarousel from '../carousel/EventsCarousel'
+import Header from '../header/Header'
+import SpinningBanner from '../spinningBanner/SpinningBaner'
+import { useState, useEffect } from 'react'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 function App() {
-  const MapsApiKey = import.meta.env.VITE_MAPS_API_KEY;
-  const url = import.meta.env.VITE_URL;
-  let [events, setEvents] = useState();
-  let [changeInEvents, setchangeInEvents] = useState(true);
+    // const MapsApiKey = import.meta.env.VITE_MAPS_API_KEY
+    const url = import.meta.env.VITE_URL
+    let [events, setEvents] = useState()
+    let [changeInEvents, setchangeInEvents] = useState(true)
+    const [filteredEvents, setFilteredEvents] = useState()
+    const [currentCategory, setCurrentCategory] = useState('Art')
+    const eventCategories = [
+        'Art',
+        'Comedy',
+        'Dance',
+        'Film',
+        'Fitness',
+        'Food',
+        'Music',
+        'Party',
+        'Sports',
+    ]
 
-  useEffect(() => {
-    if (changeInEvents) fetchEvents();
-    setchangeInEvents(false);
-  }, [events]);
+    useEffect(() => {
+        if (changeInEvents) {
+            fetchEvents()
+            fetchEventsWithCategory(currentCategory)
+        }
+        setchangeInEvents(false)
+    }, [events, currentCategory])
 
-  const fetchEvents = () => {
-    fetch(`${url}/event`)
-      .then((response) => response.json())
-      .then((data) => {
-        setEvents(data);
-      })
-      .catch((error) => console.error("Error fetching:", error));
-  };
+    const fetchEvents = () => {
+        fetch(`${url}/event`)
+            .then((response) => response.json())
+            .then((data) => {
+                setEvents(data)
+            })
+            .catch((error) => console.error('Error fetching:', error))
+    }
 
-  return (
-    <div>
-      <Header />
+    const fetchEventsWithCategory = (category) => {
+        fetch(`${url}/event/category/${category}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setFilteredEvents(data)
+            })
+            .catch((error) => console.error('Error fetching:', error))
+    }
 
-      <div className="mt-[10rem]">
-        <SpinningBanner />
-      </div>
+    return (
+        <div>
+            <Header />
 
-      <div className="mx-[5rem]  mt-[10rem] content-center sm:mx-[7rem]">
-        <h2 className="font-bebas text-white text-xl">For You</h2>
-        <EventsCarousel events={events} />
-      </div>
+            <div className="mt-[10rem]">
+                <SpinningBanner />
+            </div>
 
-      <div className="mx-[5rem]  mt-[10rem] content-center sm:mx-[7rem]">
-        <h2 className="font-bebas text-white text-xl">San Francisco</h2>
-        <EventsCarousel events={events} />
-      </div>
+            <div className="mx-[5rem]  mt-[10rem] content-center sm:mx-[7rem]">
+                <h2 className="font-bebas text-white text-xl">For You</h2>
+                <EventsCarousel events={events} />
+            </div>
 
-      <div className="mx-[5rem]  mt-[10rem] content-center sm:mx-[7rem]">
-        <h2 className="font-bebas text-white text-xl">This week</h2>
-        <EventsCarousel events={events} />
-      </div>
-    </div>
-  );
+            <div className="mx-[5rem]  mt-[10rem] content-center sm:mx-[7rem]">
+                <div>
+                    <h2 className="font-bebas text-white text-xl">
+                        Filter By Category
+                    </h2>
+                    <Menu
+                        as="div"
+                        className="relative inline-block text-left mb-[1rem]"
+                    >
+                        <div>
+                            <MenuButton className="inline-flex w-fill justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                {currentCategory}
+                                <ChevronDownIcon
+                                    aria-hidden="true"
+                                    className="-mr-1 h-5 w-5 text-gray-400"
+                                />
+                            </MenuButton>
+                        </div>
+
+                        <MenuItems
+                            transition
+                            className="absolute right-0 z-10 mt-2 w-fit origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                        >
+                            <div className="py-1">
+                                {eventCategories.map((c) => (
+                                    <MenuItem key={c}>
+                                        <button
+                                            type="button"
+                                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 w-full"
+                                            onClick={() => {
+                                                setCurrentCategory(c)
+                                                setchangeInEvents(true)
+                                            }}
+                                        >
+                                            {c}
+                                        </button>
+                                    </MenuItem>
+                                ))}
+                            </div>
+                        </MenuItems>
+                    </Menu>
+                </div>
+                <EventsCarousel events={filteredEvents} />
+            </div>
+
+            <div className="mx-[5rem]  mt-[10rem] content-center sm:mx-[7rem]">
+                <h2 className="font-bebas text-white text-xl">This week</h2>
+                <EventsCarousel events={events} />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
