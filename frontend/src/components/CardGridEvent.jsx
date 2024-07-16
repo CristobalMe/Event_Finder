1c69a6b40e44fdf4afc930cf50b61c5402cf191d
Cristobal Medina Meza <112215849+CristobalMe@users.noreply.github.com>
2024-07-16 12:58:18 -0700

    Bugs 3 (#36)
    * Fixed bugs mentioned in PR 26
    
    * Fixed bugs from PRs 26-29

--- frontend/src/components/CardGridEvent.jsx
+++ frontend/src/components/CardGridEvent.jsx
@@ -1,21 +1,15 @@
 import CardEvent from './CardEvent'
 
 const CardGridEvent = (data) => {
-    let eventData = []
-    if (data.data != undefined) {
-        eventData = data.data
-    }
     return (
         <div>
-            {eventData.length > 0 && (
-                <div className="grid lg:grid-cols-4 sm:grid-cols-2 ">
-                    {eventData.map((d) => (
-                        <div className="m-[3rem]" key={d.id}>
-                            <CardEvent data={d} />
-                        </div>
-                    ))}
-                </div>
-            )}
+            <div className="grid lg:grid-cols-4 sm:grid-cols-2 ">
+                {data.data.map((d) => (
+                    <div className="m-[3rem]" key={d.id}>
+                        <CardEvent data={d} />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
