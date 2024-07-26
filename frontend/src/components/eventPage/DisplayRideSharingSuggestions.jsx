d72dffc94798f2a53e648442c88ede6795e6a87f
Cristobal Medina Meza <112215849+CristobalMe@users.noreply.github.com>
2024-07-25 21:46:06 -0700

    Added display user preferences

--- frontend/src/components/eventPage/DisplayRideSharingSuggestions.jsx
+++ frontend/src/components/eventPage/DisplayRideSharingSuggestions.jsx
@@ -50,7 +50,7 @@ const DisplayRideSharingSuggestions = ({ user, event, preferences }) => {
                         ⊛ {rideshare.seatsAvailable} seats available
                     </p>
                     <p className="bg-white h-fit p-[.2rem] break-all">
-                        ⊛ Departing date:{' '}
+                        ⊛ Arrival date:{' '}
                         {rideshare.departingTime.substring(0, 10)}
                     </p>
                     <p className="bg-white h-fit p-[.2rem] break-all">
@@ -72,36 +72,62 @@ const DisplayRideSharingSuggestions = ({ user, event, preferences }) => {
                     </button>
                 </div>
 
-                <div className="hidden lg:flex lg:flex-col mx-5 mt-10 justify-center">
+                <div className="hidden lg:flex lg:flex-col mx-5 justify-center">
                     <EventMap
                         latitude={rideshare.departingLat}
                         longitude={rideshare.departingLong}
                     />
                 </div>
-                {/* Map and route */}
             </div>
         ))
     }
 
     return (
-        <div className="rounded overflow-hidden shadow-lg bg-white h-fit md:w-[50rem] pb-[3rem] xs:w-[20rem]">
-            {true && (
+        <div>
+            <div className="rounded overflow-hidden shadow-lg bg-white h-fit md:w-[50rem] pb-[3rem] xs:w-[20rem]">
                 <div>
                     <div className="flex items-center justify-center m-[1rem] ">
                         <div className="inline m-[1rem]">
-                            <h2 className="font-bebas text-3xl">
-                                Suggested Rideshares
-                            </h2>
+                            <h2 className="font-bebas text-3xl">Rideshares</h2>
                         </div>
                     </div>
 
+                    <section className="ridesharings-grid mb-5">
+                        <h2 className="font-bebas text-xl ml-5">
+                            Current preferences
+                        </h2>
+                        <div className="ml-5">
+                            <p className="bg-white h-fit p-[.2rem] break-all ml-5">
+                                ⊛ Number of seats needed:{' '}
+                                {preferences.numberOfSeatsNeeded}
+                            </p>
+                            <p className="bg-white h-fit p-[.2rem] break-all ml-5">
+                                ⊛ Preffered arrival date:{' '}
+                                {preferences.preferedArrivalTime.substring(
+                                    0,
+                                    10
+                                )}
+                            </p>
+                            <p className="bg-white h-fit p-[.2rem] break-all ml-5">
+                                ⊛ Preffered arrival time:{' '}
+                                {preferences.preferedArrivalTime.substring(
+                                    11,
+                                    16
+                                )}
+                            </p>
+                        </div>
+                    </section>
+
                     {userRidesharesSuggestions && (
                         <section className="ridesharings-grid">
+                            <h2 className="font-bebas text-xl ml-5">
+                                Suggested
+                            </h2>
                             {renderRidesharings(userRidesharesSuggestions)}
                         </section>
                     )}
                 </div>
-            )}
+            </div>
         </div>
     )
 }
